6e3658564a18c7164b311d9d1a68fb166593c3fd
ayesha604 <halim.aye64@gmail.com>
2025-03-08 15:30:40 -0800

    espresso

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    SAMPLE_RATE: 44100,
+    PITCH_TOLERANCE: 30, // Hz tolerance for correct pitch
+    DEFAULT_EXPECTED_PITCH: 440, // Default expected pitch (A4)
+    LYRICS_FILE: ".songs/espresso/espresso-clip.lrc"
+};
