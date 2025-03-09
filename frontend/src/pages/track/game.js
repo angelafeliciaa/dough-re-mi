50d3aac0416f5925fca87e6c4c0e6adcb20af9a1
WiDSatUBC <marketing.widsatubc@gmail.com>
2025-03-08 18:37:59 -0800

    racetrack

--- frontend/src/pages/track/game.js
+++ frontend/src/pages/track/game.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import trackImage from '../../assets/pastryicons/racetrack.png';
+import "./game.css"
 
 const Game = () => {
   return (
-    <div>
-      game
+    <div className="game-container">
+      <img src={trackImage} alt="Track Image" className="center-image" />
     </div>
-  )
-}
+  );
+};
 
-export default Game
+export default Game;
