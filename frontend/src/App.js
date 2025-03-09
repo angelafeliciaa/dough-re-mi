98ef1681dc13a1ca7245ffca066966b3d76cdd6e
WiDSatUBC <marketing.widsatubc@gmail.com>
2025-03-08 18:19:51 -0800

    pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Start from './pages/startPage/start';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Game from './pages/track/game';
 
 function App() {
   return (
-    <div className="App">
-      <Start />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ frontend/src/pages/track/car.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const car = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default car
