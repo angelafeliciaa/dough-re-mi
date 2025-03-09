0cdcdad199cf15ab4206f0ac01a7543aac191fee
Praj-na <prajnapn36@gmail.com>
2025-03-08 21:11:19 -0800

    latest

--- backend/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    SAMPLE_RATE: 44100,
-    PITCH_TOLERANCE: 30, // Hz tolerance for correct pitch
-    DEFAULT_EXPECTED_PITCH: 440, // Default expected pitch (A4)
-    LYRICS_FILE: ".songs/espresso/espresso-clip.lrc"
-};
